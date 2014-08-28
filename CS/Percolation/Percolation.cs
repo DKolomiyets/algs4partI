58863beacf7cf2a73a65c93c04e46e261dcbb28b
Dmitry Kolomiyets <D.Kolomiyets@mail.ru>
2014-08-28 03:13:39 +0300

    Percolation source files and tests added.

--- CS/Percolation/Percolation.cs
+++ CS/Percolation/Percolation.cs
@@ -1,12 +1,242 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Percolation
 {
     public class Percolation
     {
+        private bool[] sites;
+        private int size;
+        private WeightedQuickUnionUF relations;
+        private WeightedQuickUnionUF toTopSiteRelations;
+        private int topSiteIndex;
+        private int bottomSiteIndex;
+
+        public Percolation( int n )
+        {
+            if( n <= 0 )
+            {
+                throw new ArgumentException( "Invalid grid size specified." );
+            }
+
+            size = n;
+            sites = new bool[size * size + 1];
+
+            for( int i = 0; i < size * size + 1; i++ )
+                sites[i] = false;
+
+            relations = new WeightedQuickUnionUF( size * size + 2 );
+            topSiteIndex = 0;
+            bottomSiteIndex = size * size + 1;
+
+            toTopSiteRelations = new WeightedQuickUnionUF( size * size + 1 );
+        }
+
+        public bool Percolates()
+        {
+            return relations.Connected( topSiteIndex, bottomSiteIndex );
+        }
+
+        public void Open( int i, int j )
+        {
+            CheckIndexes( i, j );
+
+            int index = XyTo1D( i, j );
+
+            sites[index] = true;
+
+            if( size == 1 )
+            {
+                relations.Union( topSiteIndex, index );
+                relations.Union( bottomSiteIndex, index );
+                toTopSiteRelations.Union( topSiteIndex, index );
+            }
+            else if( i == 1 && j == 1 )
+            {
+                if( IsOpen( i + 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i + 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i + 1, j ) );
+                }
+                if( IsOpen( i, j + 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j + 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j + 1 ) );
+                }
+                relations.Union( topSiteIndex, index );
+                toTopSiteRelations.Union( topSiteIndex, index );
+            }
+            else if( i == size && j == size )
+            {
+                if( IsOpen( i - 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i - 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i - 1, j ) );
+                }
+                if( IsOpen( i, j - 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j - 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j - 1 ) );
+                }
+                relations.Union( bottomSiteIndex, index );
+            }
+            else if( i == size && j == 1 )
+            {
+                if( IsOpen( i - 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i - 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i - 1, j ) );
+                }
+                if( IsOpen( i, j + 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j + 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j + 1 ) );
+                }
+                relations.Union( bottomSiteIndex, index );
+            }
+            else if( i == 1 && j == size )
+            {
+                if( IsOpen( i + 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i + 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i + 1, j ) );
+                }
+                if( IsOpen( i, j - 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j - 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j - 1 ) );
+                }
+                relations.Union( topSiteIndex, index );
+                toTopSiteRelations.Union( topSiteIndex, index );
+            }
+            else if( i == 1 )
+            {
+                if( IsOpen( i + 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i + 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i + 1, j ) );
+                }
+                if( IsOpen( i, j + 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j + 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j + 1 ) );
+                }
+                if( IsOpen( i, j - 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j - 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j - 1 ) );
+                }
+                relations.Union( topSiteIndex, index );
+                toTopSiteRelations.Union( topSiteIndex, index );
+            }
+            else if( i == size )
+            {
+                if( IsOpen( i - 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i - 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i - 1, j ) );
+                }
+                if( IsOpen( i, j + 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j + 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j + 1 ) );
+                }
+                if( IsOpen( i, j - 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j - 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j - 1 ) );
+                }
+                relations.Union( bottomSiteIndex, index );
+            }
+            else if( j == 1 )
+            {
+                if( IsOpen( i - 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i - 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i - 1, j ) );
+                }
+                if( IsOpen( i + 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i + 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i + 1, j ) );
+                }
+                if( IsOpen( i, j + 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j + 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j + 1 ) );
+                }
+            }
+            else if( j == size )
+            {
+                if( IsOpen( i - 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i - 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i - 1, j ) );
+                }
+                if( IsOpen( i + 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i + 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i + 1, j ) );
+                }
+                if( IsOpen( i, j - 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j - 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j - 1 ) );
+                }
+            }
+            else
+            {
+                if( IsOpen( i - 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i - 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i - 1, j ) );
+                }
+                if( IsOpen( i + 1, j ) )
+                {
+                    relations.Union( index, XyTo1D( i + 1, j ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i + 1, j ) );
+                }
+                if( IsOpen( i, j + 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j + 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j + 1 ) );
+                }
+                if( IsOpen( i, j - 1 ) )
+                {
+                    relations.Union( index, XyTo1D( i, j - 1 ) );
+                    toTopSiteRelations.Union( index, XyTo1D( i, j - 1 ) );
+                }
+            }
+        }
+
+        public bool IsOpen( int i, int j )
+        {
+            CheckIndexes( i, j );
+
+            int index = XyTo1D( i, j );
+
+            return sites[index];
+        }
+
+        public bool IsFull( int i, int j )
+        {
+            CheckIndexes( i, j );
+
+            return IsOpen( i, j ) && toTopSiteRelations.Connected( topSiteIndex, XyTo1D( i, j ) );
+        }
+
+        private int XyTo1D( int x, int y )
+        {
+            return ( x - 1 ) * size + y;
+        }
+
+        private void CheckIndexes( int x, int y )
+        {
+            if( x <= 0 || x > size )
+                throw new ArgumentOutOfRangeException();
+
+            if( y <= 0 || y > size )
+                throw new ArgumentOutOfRangeException();
+        }
     }
+
 }
---
+++ CS/Percolation/PercolationTests.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Percolation
+{
+    [TestClass]
+    public class PercolationTests
+    {
+        [TestMethod]
+        public void TestThat1DimensionalMatrixPercolatesAfterOneOpen()
+        {
+            Percolation target = new Percolation( 1 );
+
+            target.Open( 1, 1 );
+
+            Assert.IsTrue( target.Percolates() );
+        }
+
+        [TestMethod]
+        public void TestDoesNotPercolate2DimensionalMatrixAfterCreation()
+        {
+            Percolation target = new Percolation( 2 );
+
+            Assert.IsFalse( target.Percolates() );
+        }
+
+        [TestMethod]
+        public void TestIsNotOpenAfterCreation()
+        {
+            Percolation target = new Percolation( 2 );
+
+            Assert.IsFalse( target.IsOpen( 1, 1 ) );
+        }
+
+        [TestMethod]
+        public void TestIsNotFullClosedSite()
+        {
+            Percolation target = new Percolation( 2 );
+
+            Assert.IsFalse( target.IsFull( 1, 1 ) );
+        }
+
+        [TestMethod]
+        public void TestIsNotFull()
+        {
+            Percolation target = new Percolation( 2 );
+
+            target.Open( 2, 2 );
+
+            Assert.IsFalse( target.IsFull( 2, 2 ) );
+        }
+
+        [TestMethod]
+        public void TestIsFull()
+        {
+            Percolation target = new Percolation( 2 );
+
+            target.Open( 1, 1 );
+
+            Assert.IsTrue( target.IsFull( 1, 1 ) );
+        }
+
+        [TestMethod]
+        public void TestIsOpenAfterOpen()
+        {
+            Percolation target = new Percolation( 2 );
+
+            target.Open( 1, 1 );
+
+            Assert.IsTrue( target.IsOpen( 1, 1 ) );
+        }
+
+        [TestMethod]
+        public void TestPercolates2DimensionalMatrix()
+        {
+            Percolation target = new Percolation( 2 );
+
+            target.Open( 1, 1 );
+            target.Open( 2, 1 );
+
+            Assert.IsTrue( target.Percolates() );
+        }
+
+        [TestMethod]
+        public void TestDoesNotPercolate2DimensionalMatrix()
+        {
+            Percolation target = new Percolation( 2 );
+
+            target.Open( 1, 1 );
+            target.Open( 2, 2 );
+
+            Assert.IsFalse( target.Percolates() );
+        }
+
+        [TestMethod]
+        public void TestPercolates3DimensionalMatrix()
+        {
+            Percolation target = new Percolation( 3 );
+
+            target.Open( 1, 1 );
+            target.Open( 2, 1 );
+            target.Open( 3, 1 );
+
+            Assert.IsTrue( target.Percolates() );
+        }
+
+        [TestMethod]
+        public void TestDoesNotPercolate3DimensionalMatrix()
+        {
+            Percolation target = new Percolation( 3 );
+
+            target.Open( 1, 1 );
+            target.Open( 2, 2 );
+            target.Open( 3, 3 );
+
+            Assert.IsFalse( target.Percolates() );
+        }
+
+        [TestMethod]
+        public void TestPercolates4DimensionalMatrix()
+        {
+            Percolation target = new Percolation( 4 );
+
+            target.Open( 1, 1 );
+            target.Open( 2, 1 );
+            target.Open( 3, 1 );
+            target.Open( 4, 1 );
+
+            Assert.IsTrue( target.Percolates() );
+        }
+
+        [TestMethod]
+        public void TestDoesNotPercolate4DimensionalMatrix()
+        {
+            Percolation target = new Percolation( 4 );
+
+            target.Open( 1, 1 );
+            target.Open( 2, 2 );
+            target.Open( 3, 3 );
+            target.Open( 4, 4 );
+
+            Assert.IsFalse( target.Percolates() );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatOpenShouldThrowExceptionOnZeroIndexes()
+        {
+            Percolation target = new Percolation( 4 );
+
+            target.Open( 0, 0 );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatOpenShouldThrowExceptionOnGreaterThenSizeIndexSpecified()
+        {
+            Percolation target = new Percolation( 4 );
+
+            target.Open( 5, 1 );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatIsOpenShouldThrowExceptionOnGreaterThenSizeIndexSpecified()
+        {
+            Percolation target = new Percolation( 4 );
+            
+            target.IsOpen( 5, 1 );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatIsOpenShouldThrowExceptionOnZeroIndexSpecified()
+        {
+            Percolation target = new Percolation( 4 );
+
+            target.IsOpen( 0, 0 );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatIsFullShouldThrowExceptionOnGreaterThenSizeIndexSpecified()
+        {
+            Percolation target = new Percolation( 4 );
+
+            target.IsFull( 5, 1 );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatIsFullShouldThrowExceptionOnZeroIndexSpecified()
+        {
+            Percolation target = new Percolation( 4 );
+
+            target.IsFull( 0, 0 );
+        }
+
+        [TestMethod]
+        public void TestIsNotFullEvenIfSiteIsOpenAndSystemPercolates()
+        {
+            Percolation target = new Percolation( 3 );
+
+            target.Open( 3, 1 );
+
+            target.Open( 1, 3 );
+            target.Open( 2, 3 );
+            target.Open( 3, 3 );
+
+            Assert.IsFalse( target.IsFull( 3, 1 ) );
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestIfIllegalArgumentExceptionThrownOnZeroSize()
+        {
+            new Percolation( 0 );
+        }
+    }
+}
